4860003c370c0427d257662368ef700e652d71e3
Fredrik Welander <welandfr@arcada.fi>
2025-01-28 23:29:04 +0200

    Add method not allowed handling

--- api/bikes/index.js
+++ api/bikes/index.js
@@ -1,17 +1,17 @@
 export default async function handler(req, res) {
 
     if (req.method == "POST") {
-        res.status(200).json({status: `Created new bike: ${req.body.brand}`});
+        return res.status(200).json({status: `Created new bike: ${req.body.brand}`});
     }
 
     if (req.method == "GET") { 
 
-        res.status(200).json([
+        return res.status(200).json([
             { brand: "Nishiki", model: "Road Master", price: 400 },
             { brand: "Scott", model: "Peak", price: 500 }
         ]);
     }
 
-    
+    res.status(405).json({ error: 'Method not allowed' });
 
 }
